ba677d761a18cecb0a322bb8188d0dc3fbf80b61
hr5t5@windowslive.com <hr5t5@windowslive.com>
2019-03-06 09:42:56 +0100

    fix naming and handle errors

--- src/app/helpers/error.interceptor.ts
+++ src/app/helpers/error.interceptor.ts
@@ -3,15 +3,22 @@ import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/c
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import {AuthService} from '../services/auth.service';
+import {Router} from '@angular/router';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
-    constructor(private authenticationService: AuthService) { }
+    constructor(private authenticationService: AuthService, private router: Router) { }
 
     public intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         return next.handle(request).pipe(catchError(err => {
             if (err.status === 401) {
                 this.authenticationService.logout();
+                this.authenticationService.loading = false;
+                this.router.navigate(['/sign-in']);
+            }
+            if (err.status === 400) {
+                // TODO make dialog box
+                alert('Login error!');
                 this.authenticationService.loading = false;
             }
 
