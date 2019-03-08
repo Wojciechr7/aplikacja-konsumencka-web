d36fa6cc1158a22b9011bbb1b31cf266df7c1adb
matszc <33006487+matszc@users.noreply.github.com>
2019-03-08 20:16:27 +0100

    create ad adding prototype

--- src/app/components/add-ad/add-ad.component.ts
+++ src/app/components/add-ad/add-ad.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddAdComponent implements OnInit {
 
+  caregories = ['Apartment', 'Room', 'House', 'Office'];
+  types = ['rent', 'sale'];
+
   constructor() { }
 
   ngOnInit() {
