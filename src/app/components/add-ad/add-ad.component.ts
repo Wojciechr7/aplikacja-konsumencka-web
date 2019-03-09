4688942e0c66d59f3df5e9dba545c3188a5060f5
matszc <33006487+matszc@users.noreply.github.com>
2019-03-09 22:42:27 +0100

    add validation

--- src/app/components/add-ad/add-ad.component.ts
+++ src/app/components/add-ad/add-ad.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, Validators, FormGroup} from '@angular/forms';
 
 
 @Component({
@@ -8,29 +8,27 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./add-ad.component.scss']
 })
 export class AddAdComponent implements OnInit {
-
+  AdForm: FormGroup;
   caregories = ['Apartment', 'Room', 'House', 'Office'];
   types = ['rent', 'sale'];
-
-  city: string;
-
-  disable = true;
-
-  TittleFormControl = new FormControl('', [
-    Validators.required,
-  ]);
-
-  cityCheck() {
-    if (this.city.length > 1) {
-      this.disable = false;
-    } else {
-      this.disable = true;
-    }
-  }
 
   constructor() { }
 
   ngOnInit() {
+    this.AdForm = new FormGroup ({
+      TittleFormControl:  new FormControl('', [Validators.required, Validators.minLength(5)]),
+      CaregorieFormControl:  new FormControl('', [Validators.required]),
+      CityFormControl:  new FormControl('', [Validators.required]),
+      NeighborFormControl:  new FormControl(''),
+      StreetFormContorl:  new FormControl(''),
+      PhoneNumberFormContorl:  new FormControl('', [Validators.required]),
+      SizeFormControl:  new FormControl(''),
+      FloorFormControl:  new FormControl(''),
+      PriceFomrControl:  new FormControl('', [Validators.required]),
+      TypeFormControl:  new FormControl('', [Validators.required]),
+      DiscriptionFormControl:  new FormControl('', [Validators.required]),
+    });
   }
+  get FormCon(): any { return this.AdForm.get('TittleFormControl'); }
 
 }
