69547b7b4d803505775f5306cca155a93383bc14
zbipaw <xantimage@gmail.com>
2019-03-10 18:57:30 +0100

    added temporary ad model

---
+++ src/app/models/ad.ts
@@ -0,0 +1,13 @@
+export interface AdData {
+    TitleFormControl: string;
+    CategoryFormControl: string;
+    CityFormControl: string;
+    NeighbourFormControl: string;
+    StreetFormControl: string;
+    PhoneNumberFormControl: string;
+    SizeFormControl: string;
+    FloorFormControl: string;
+    PriceFormControl: string;
+    TypeFormControl: string;
+    DescriptionFormControl: string;
+}
