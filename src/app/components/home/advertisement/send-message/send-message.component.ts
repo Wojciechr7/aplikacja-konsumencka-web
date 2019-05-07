e339a6d0d11d8d322443568a8463c378a16692c1
hr5t5@windowslive.com <hr5t5@windowslive.com>
2019-05-07 10:49:49 +0200

    hotfix error while trying to send message by not signed in users

--- src/app/components/home/advertisement/send-message/send-message.component.ts
+++ src/app/components/home/advertisement/send-message/send-message.component.ts
@@ -1,14 +1,14 @@
-import {Ad} from './../../../../models/ad';
 import {SentMessage} from '../../../../models/conversation/sent-message';
-import {MessageService} from './../../../../services/message.service';
 import {Component, OnInit, Input} from '@angular/core';
-import {FormControl, Validators, FormGroup, FormBuilder} from '@angular/forms';
+import {Validators, FormGroup, FormBuilder} from '@angular/forms';
 import {Observable} from 'rxjs';
 import {ToastrService} from 'ngx-toastr';
 import {AuthService} from '../../../../services/auth.service';
 import {MatDialogRef} from '@angular/material';
 import {MessageDialogComponent} from '../../../../dialogs/message/message.dialog';
 import {SignalRService} from '../../../../services/signal-r.service';
+import {Ad} from '../../../../models/ad';
+import {MessageService} from '../../../../services/message.service';
 
 @Component({
     selector: 'app-send-message',
@@ -39,14 +39,18 @@ export class SendMessageComponent implements OnInit {
             };
             if (!this.senderId) {
                 this.userId.subscribe((ad: Ad) => {
-                    if (this.authenticationService.currentUserValue.id !== ad.userId) {
-                        this.signalR.sendDirectMessage(ad.userId, message as SentMessage);
-                        this.messageService.sendMessage(ad.userId, message as SentMessage).subscribe(() => {
-                            this.toastr.success('Message Has Been Sent', 'Success!');
-                            this.messageForm.get('text').setValue('');
-                        });
+                    if (this.authenticationService.currentUserValue) {
+                        if (this.authenticationService.currentUserValue.id !== ad.userId) {
+                            this.signalR.sendDirectMessage(ad.userId, message as SentMessage);
+                            this.messageService.sendMessage(ad.userId, message as SentMessage).subscribe(() => {
+                                this.toastr.success('Message Has Been Sent', 'Success!');
+                                this.messageForm.get('text').setValue('');
+                            });
+                        } else {
+                            this.toastr.error('You Can Not Send The Message To Yourself', 'Error!');
+                        }
                     } else {
-                        this.toastr.error('You Can Not Send The Message To Yourself', 'Error!');
+                        this.toastr.warning('You Must Sign In To Send Messages', 'Warning!');
                     }
                 });
             } else {
